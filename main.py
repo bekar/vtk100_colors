bff936e7fcba1ecc584cc6d844e77be004ea6873
rho <rho.rhoit@gmail.com>
2014-06-19 00:24:58 +0545

    update and clean up
    * auto get font
    * using text.index

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from tkinter import *
-
-def_font=[ "DejaVuSansMono", 11, "normal" ]
+import os, sys
+import tkinter
 
 pallet8 = [
     "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white",
@@ -16,20 +15,21 @@ pallet16 = [
 
 xx = [ "00", "5f", "87", "af", "d7", "ff" ]
 
-import os
+SGR = True
 try:
-    if os.environ['SGR'].lower() in [ "false", "0" ]: SGR=False
-    else: SGR=True
+    if os.environ['SGR'].lower() in [ "false", "0" ]:
+        SGR = False
 except Exception as NoEnvVariableSet:
-    SGR=True
+    pass
 
 class vt100tk():
-    def __init__(self, txt_wig, string=None):
-        self.txtwig=txt_wig
-        self.txtwig.tag_config(1, font=def_font[:2] + ["bold"])
-        self.txtwig.tag_config(3, font=def_font[:2] + ["italic"])
-        self.txtwig.tag_config(4, underline=TRUE)
-        self.txtwig.tag_config(9, overstrike=TRUE)
+    def __init__(self, text_wig, string=None):
+        self.txtwig = text_wig
+        _font = self.txtwig['font'].split()
+        self.txtwig.tag_config(1, font=_font[:2] + ["bold"])
+        self.txtwig.tag_config(3, font=_font[:2] + ["italic"])
+        self.txtwig.tag_config(4, underline=1)
+        self.txtwig.tag_config(9, overstrike=1)
         for i in range(8): # pallet8
             self.txtwig.tag_config(i+30, foreground=pallet8[i])
             self.txtwig.tag_config(i+40, background=pallet8[i])
@@ -39,62 +39,62 @@ class vt100tk():
         for i in range(6): # 16-231/256-colors
             for j in range(6):
                 for k in range(6):
-                    rgb="#"+xx[i]+xx[j]+xx[k]
-                    suffix=str(i*36+j*6+k+16)
+                    rgb = "#"+xx[i]+xx[j]+xx[k]
+                    suffix = str(i*36+j*6+k+16)
                     self.txtwig.tag_config("fg"+suffix, foreground=rgb)
                     self.txtwig.tag_config("bg"+suffix, background=rgb)
         for i in range(24): # 232-255/256-colors
-            value=hex(i*10+8); rgb="#"+value[2:]*3
-            suffix=str(i+232)
+            value = hex(i*10+8); rgb = "#"+value[2:]*3
+            suffix = str(i+232)
             self.txtwig.tag_config("fg"+suffix, foreground=rgb)
             self.txtwig.tag_config("bg"+suffix, background=rgb)
         if string: self.parser(string)
 
-    def tag_sgr(self, code, pre, cur):
+    def tag_sgr(self, code):
         if not code: return None
-        tag=int(code);
+        tag = int(code);
         if tag == 0: return None
-        if   self.extend==53: tag="bg"+code; self.extend=0; # 38+5+code
-        elif self.extend==43: tag="fg"+code; self.extend=0; # 48+5+code
-        elif self.extend: self.extend+=tag; return; #2nd skip
-        elif tag in [ 38, 48 ]: self.extend=tag; return;
-        self.txtwig.tag_add(tag, pre, cur)
+        if   self.ext == 53: tag = "bg" + code; self.ext = 0; # 38+5+code
+        elif self.ext == 43: tag = "fg" + code; self.ext = 0; # 48+5+code
+        elif self.ext: self.ext += tag; return; #2nd skip
+        elif tag in [ 38, 48 ]: self.ext = tag; return;
+        self.txtwig.tag_add(tag, self.pre, self.cur)
         return tag
 
-    def de_code(self, fp, pre, cur):
-        self.extend=0; fbreak=fp
+    def de_code(self, fp):
+        self.ext = 0; fbreak = fp
         while self.string[fp] != 'm':
-            if self.string[fp]==";":
-                self.tag_sgr(self.string[fbreak:fp], pre, cur);
-                fbreak=fp+1
-            fp+=1
-        return self.tag_sgr(self.string[fbreak:fp], pre, cur)
+            if self.string[fp] == ";":
+                self.tag_sgr(self.string[fbreak:fp])
+                fbreak = fp + 1
+            fp += 1
+        return self.tag_sgr(self.string[fbreak:fp])
 
     def parser(self, string):
-        self.txtwig.delete('1.0', END)
-        cur=""; j=1; i=fp=cflag=code=0
-        length=len(string)
-        self.string=string
-        while fp<length:
+        self.cur = "";
+        fp = cflag = code = 0
+        length = len(string)
+        self.string = string
+        while fp < length:
             if string[fp]=='\x1b':
-                pre=cur; cur=str(j)+'.'+str(i)
-                pcode=code; code=fp+2 # +2 shift escape sequence
-                while string[fp]!="m": fp+=1
-                fp+=1; cflag+=1
-                if cflag==2 and SGR:
-                    self.de_code(pcode, pre, cur); cflag-=1;
-                continue # for fp>strlen
-            if string[fp]=='\n': j+=1; i=-1; #print()
-            self.txtwig.insert(END, string[fp])
-            fp+=1; i+=1
+                self.pre = self.cur
+                self.cur = self.txtwig.index(tkinter.CURRENT)
+                pcode = code + 2; code = fp # +2 shift escape sequence
+                while string[fp] !="m": fp += 1
+                fp += 1; cflag += 1
+                if cflag == 2 and SGR:
+                    self.de_code(pcode); cflag-=1;
+                continue
+            self.txtwig.insert("end", string[fp])
+            fp += 1;
 
 if __name__ == "__main__" :
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print("Argument(s) Missing", file=sys.stderr); exit(1);
-    root=Tk()
-    text=Text(root, font=def_font)
+
     from subprocess import check_output
-    vtk=vt100tk(text, check_output(sys.argv[1:], universal_newlines=True))
-    text.pack(expand=YES, fill=BOTH)
-    root.bind("<Key-Escape>", lambda event: quit())
-    root.mainloop()
+    text = tkinter.Text(font=[ "DejaVuSansMono", 11, "normal" ])
+    text.pack(expand=1, fill="both")
+    vt100tk(text, check_output(sys.argv[1:], universal_newlines=True))
+    text.master.bind("<Key-Escape>", lambda event: quit())
+    tkinter.mainloop()
